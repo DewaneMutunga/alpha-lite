1cd59861d3bc776527cdcbd7f0020cfd2e029c33
Dewane Mutunga <dewane.mutunga@gmail.com>
2014-04-30 00:53:15 -0400

    Add full-width structure

--- footer.php
+++ footer.php
@@ -8,17 +8,22 @@
  */
 ?>
 
-	</div><!-- #content -->
+			</div><!-- #content -->
+		</div>
+	</div>
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'alpha' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'alpha' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'alpha' ), 'Alpha', '<a href="http://dewanemutunga.com" rel="designer">Dewane Mutunga</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
+	<div id="footer-area" class="full">
+		<div class="main">
+			<footer id="colophon" class="site-footer inner" role="contentinfo">
+				<div class="site-info">
+					<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'alpha' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'alpha' ), 'WordPress' ); ?></a>
+					<span class="sep"> | </span>
+					<?php printf( __( 'Theme: %1$s by %2$s.', 'alpha' ), 'Alpha', '<a href="http://dewanemutunga.com" rel="designer">Dewane Mutunga</a>' ); ?>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div>
+	</div>
+	
 <?php wp_footer(); ?>
 
 </body>
--- header.php
+++ header.php
@@ -19,20 +19,24 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+	<div id="header-area" class="full">
+		<div class="main">
+			<header id="masthead" class="site-header inner" role="banner">
+				<div class="site-branding">
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+				</div>
+		
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<button class="menu-toggle"><?php _e( 'Primary Menu', 'alpha' ); ?></button>
+					<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'alpha' ); ?></a>
+		
+					<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+				</nav><!-- #site-navigation -->
+			</header><!-- #masthead -->
 		</div>
+	</div>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Primary Menu', 'alpha' ); ?></button>
-			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'alpha' ); ?></a>
-
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+	<div id="content-area" class="full">
+		<div class="main">	
+			<div id="content" class="site-content">
