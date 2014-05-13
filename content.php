0d0ba377e9ea97f638e947796f4cc4255bf49f1b
Dewane Mutunga <dewane.mutunga@gmail.com>
2014-05-13 00:00:10 -0400

    edit default content structure

--- content.php
+++ content.php
@@ -5,59 +5,26 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php 
+		if ( has_post_thumbnail() ) : ?>
+			<div class="featured-img-container">
+				<a href="<?php the_permalink(); ?>" rel="bookmark">
+					<?php the_post_thumbnail( 'content-thumb', array( 'class' => 'featured-img' ) ); ?>
+				</a>
+			</div>
+		<?php endif;
+	?>
 	<header class="entry-header">
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+	</header>
 
+	<div class="entry-summary">
+		<?php the_excerpt(); ?>
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php alpha_posted_on(); ?>
-		</div><!-- .entry-meta -->
+			<?php echo '<a class="read-more" href="'. get_permalink( get_the_ID() ) . '">Read More <i class="fa fa-arrow-circle-right"></i></a>'; ?>
+		</div>
 		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-	<?php else : ?>
-	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'alpha' ) ); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'alpha' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-	<?php endif; ?>
-
-	<footer class="entry-footer">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'alpha' ) );
-				if ( $categories_list && alpha_categorized_blog() ) :
-			?>
-			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'alpha' ), $categories_list ); ?>
-			</span>
-			<?php endif; // End if categories ?>
-
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'alpha' ) );
-				if ( $tags_list ) :
-			?>
-			<span class="tags-links">
-				<?php printf( __( 'Tagged %1$s', 'alpha' ), $tags_list ); ?>
-			</span>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
-
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'alpha' ), __( '1 Comment', 'alpha' ), __( '% Comments', 'alpha' ) ); ?></span>
-		<?php endif; ?>
-
-		<?php edit_post_link( __( 'Edit', 'alpha' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
+	</div>
+</article>
