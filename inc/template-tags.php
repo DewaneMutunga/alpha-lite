251e0fd58a51abefd0712a36b25b9e4b6bcd5860
Sean Davis <sdavis2702@gmail.com>
2014-05-28 20:40:46 -0500

    remove post footer callback function

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -143,26 +143,6 @@ function alpha_lite_category_transient_flusher() {
 add_action( 'edit_category', 'alpha_lite_category_transient_flusher' );
 add_action( 'save_post',     'alpha_lite_category_transient_flusher' );
 
-if ( ! function_exists( 'alpha_lite_author_box' ) ) :
-/**
- * Create an author box that will show show after content on single posts.
- */
-function alpha_lite_author_box() {
-	?>
-	<div class="single-post-footer clear">
-		<div class="post-footer-author">
-			<?php echo get_avatar( get_the_author_meta( 'ID' ), 32, '', get_the_author_meta( 'display_name' ) ); ?>
-			<span class="author-name"><?php echo __( 'This post was written by ', 'alpha_lite' ) . get_the_author_meta( 'display_name' ); ?></span>	
-		</div>
-		<div class="post-footer-author-bio">
-			<p><?php echo get_the_author_meta( 'description' ); ?></p>
-		</div>
-		<?php do_action( 'social_profiles' ); ?>
-	</div>
-	<?php
-}
-endif;
-
 /**
  * social profiles
  */
